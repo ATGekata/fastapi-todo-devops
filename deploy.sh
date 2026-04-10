efbf4dd3c9e74eeef39f44784d04dc05f14f172c
Monarch <admin@alexnasibulin.ru>
2026-04-10 13:47:51 +0500

    Fix port

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,5 @@ docker pull 192.168.1.66:5050/monarch/fastapi-todo-devops:latest
 docker compose down
 docker compose up -d
 
-sleep 5
-curl -f http://127.0.0.1:8001/health
+sleep 8
+curl -f http://127.0.0.1:8080/health
